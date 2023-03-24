8ca0fc456ccb24389c8e427a900f4fbef72639ad
Merritt Kowaleski <mkowales@andrew.cmu.edu>
2023-03-24 12:45:46 -0400

    add hyperopt requirement

--- setup.py
+++ setup.py
@@ -49,7 +49,8 @@ setup(
         'torchvision>=0.5.0',
         'statsforecast',
         'neuralforecast>=1.3.0',
-        'transformers>=4.6.0'
+        'transformers>=4.6.0',
+        'hyperopt'
     ],
     entry_points=read_entry_points(),
     url='https://github.com/autonlab/autonbox',
